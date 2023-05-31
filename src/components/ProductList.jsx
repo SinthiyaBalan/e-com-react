28fcb980c0e0b418d69c285e62d45a42fdc063cd
SinthiyaBalan <sinthiyamct@gmail.com>
2023-05-31 09:42:57 +0200

    first commit

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const ProductList = () => {
+
+    const [products, setProducts] = useState([])
+  return (
+    <div>
+        <h1>ProductList</h1>
+    </div>
+  )
+}
+
+export default ProductList
